e0148853c203bcdcac265eabaff93c7130f16555
wuyangbing <wuyangbing@wuyangbingdeMacBook-Pro.local>
2014-11-02 19:56:02 +0800

    Initial Commit

---
+++ journal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  journal
+//
+//  Created by wuyangbing on 14/11/2.
+//  Copyright (c) 2014年 wuyangbing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
